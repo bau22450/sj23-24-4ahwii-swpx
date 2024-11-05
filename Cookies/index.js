9078b3a85a001a9ed7c240a86e53349ba86a7d32
# <jakob.baumgartner@gmail.com>
2024-11-05 21:58:43 +0100

    Reliefe

--- Cookies/index.js
+++ Cookies/index.js
@@ -1,8 +1,12 @@
+const { PrismaClient } = require('@prisma/client');
+
+const prisma = new PrismaClient()
 var express = require("express"),
     session = require("express-session"),
+    bodyParser = require("body-parser"),
     app = express(),
     MemcachedStore = require("connect-memcached")(session);
-
+app.use(bodyParser.urlencoded({ extended: true }));
 app.use(
     session({
         secret: "hehehihi",
@@ -18,26 +22,81 @@ app.use(
 );
 
 app.get("/", function (req, res) {
-    if (req.session.username) {
 
+    if (req.session.username) {
+        res.send("Wilkommen " + req.session.username + "<a href=' /Mitglieder '>Mitglieder</a>");
     } else {
-        req.session.username = "unknown";
+        res.redirect("/loginSeite");
     }
-    res.send("Wilkommen " + req.session.username);
+
 });
 
 app.listen(9341, function () {
     console.log("Listening on %d", this.address().port);
 });
 
-app.post("/login", function (req, res) {
+app.post("/login", async function (req, res) {
     const { username, password } = req.body;
-    req.session.username = username;
-    req.session.password = password;
-    console.log(username);
+    if (await getUser(username, password)) {
+        req.session.username = username;
+        req.session.password = password;
+        res.send(`Login erfolgreich. Willkommen, ${username} du cooles kid!<br><a href="/Mitglieder">Mitglieder</a><br><a href="/logout">Logout</a><br><a href="/">Home</a>`);
+    }
+    else {
+        res.send("SOOOOOOOOOOOOO nicht");
+    }
+
 });
 
+async function getUser(username, password) {
+    try {
+        const user = await prisma.user.findUnique({
+            where: {
+                username: username,
+                password: password,
+            },
+        });
+        if (user === null) {
+            return false;
+        }
+        console.log(user);
+        return true;
+    } catch (error) {
+        console.error("Error fetching user:", error);
+    }
+}
 
 app.get("/loginSeite", function (req, res) {
-    res.sendFile(`${__dirname}/index.html`);
+    res.sendFile(`${__dirname}/login.html`);
+});
+
+app.get("/logout", function (req, res) {
+    req.session.destroy(function (err) {
+        if (err) {
+            return res.status(500).send("Logout failed.");
+        }
+        res.send("Erfolgreich ausgeloggt <a href='/'>Home</a>");
+    });
+});
+
+app.post("/register", async function (req, res) {
+    const { username, password } = req.body;
+    await prisma.User.create({
+        data: {
+            username: username,
+            password: password
+        }
+
+    })
+    req.session.username = username;
+    req.session.password = password;
+    res.send("YIPPYYYYYYYYYY " + req.session.username + " hier gehts zu den <a href=' /Mitglieder '>Coolen kids</a>");
+});
+
+app.get("/Mitglieder", function (req, res) {
+    if (req.session.username) {
+        res.send("Wilkommen bei den Coolen Kids " + req.session.username + " <a href=' /logout '>Ciao</a>");
+    } else {
+        res.redirect("/loginSeite");
+    }
 });
